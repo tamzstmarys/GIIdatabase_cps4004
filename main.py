5109b053ce56964e45e2bb6273129c60c9b22897
tamzstmarys <2405149@live.stmarys.ac.uk>
2025-04-25 22:03:08 +0100

    After connecting to the sql database, I inserted a sample customer which named after myself. I also used 'cursor.lastrowid' to get the inserted ID

---
+++ main.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+conn=sqlite3.connect("insurance.db")
+cursor = conn.cursor()
+
+customer_id = cursor.lastrowid
+cursor.execute("""
+INSERT INTO Customer (name, telephone, email)
+VALUES ('Tahmid Miah', 'tamz@outlook.com', '440252525')
+""")
